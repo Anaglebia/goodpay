c79d5d4da0e324cb49a7805fe5019cf23f0fd534
Ana <bia22@DESKTOP-EM0A4V1>
2021-12-20 18:09:46 -0300

    Adicao do modulo d dados

---
+++ src/moduloDados.js
@@ -0,0 +1,6 @@
+const userLogin=[
+    {id:0001, username:"vitoricoma", senha:"gordinho123",email:"vitor.icoma@soulcodeacademy.org"},
+    {id:0002, username:"vMarcelofarofa", senha:"ff123", email:"marceloff@gmail.com"}
+];
+
+export default userLogin;
